1b01e9f80847f78f03925996b79b1e5b9a6e1827
Saiful Islam <143730361+Saiful-Islam25@users.noreply.github.com>
2023-09-03 13:27:08 +0600

    Add files via upload

---
+++ Min.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    // Using the int data type.
+    int element1 = 10;
+    int element2 = 20;
+    // Finding the minimum value.
+    cout << min(element1, element2) << endl;    
+    
+    // It will return the first value.
+    cout << min(31, 30);    
+
+    return 0;
+}
