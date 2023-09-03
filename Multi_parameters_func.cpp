6843d21d6c913d66cce152e0d69bd4e05e03abb0
Saiful Islam <143730361+Saiful-Islam25@users.noreply.github.com>
2023-09-03 13:27:28 +0600

    Add files via upload

---
+++ Multi_parameters_func.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void func(string name,int age){
+	cout<<name<<" Sheikh, "<<age<<" years old "<<endl;
+}
+
+int main(){
+	func("Nijhum",13);
+	func("Saif",22);
+}
