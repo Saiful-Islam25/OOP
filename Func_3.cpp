abe317210cb9cf8382b66523898f520f73ab13f0
Saiful Islam <143730361+Saiful-Islam25@users.noreply.github.com>
2023-09-03 13:25:51 +0600

    Add files via upload

---
+++ Func_3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void func(string fname1,string fname2){
+	cout<<fname1<<" Sheikh "<<endl<<fname2<<" Sheikh "<<endl;
+	
+}
+
+int main(){
+
+func("Nijhum","Saif");
+
+return 0;
+}
+
