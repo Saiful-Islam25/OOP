ad09acd41b643cc282befcf20f87855cf40947c6
Saiful Islam <143730361+Saiful-Islam25@users.noreply.github.com>
2023-09-03 13:30:57 +0600

    Add files via upload

---
+++ Virtual_function.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+	public:
+		 virtual void print(){
+			cout<<"Print base class"<<endl;
+		}
+		void show(){
+			cout<<"Show base class"<<endl;
+		}
+};
+
+class Derived:public Base{
+	public:
+		void print(){
+			cout<<"Print derived class"<<endl;
+		}
+		void show(){
+			cout<<"show derived class"<<endl;
+		}
+};
+
+int main(){
+	Base *bptr;
+	Derived d;
+	bptr=&d;
+	
+	bptr->print();
+	bptr->show();
+}
