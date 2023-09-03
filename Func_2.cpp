03a98c032d09bc65ae604fc03ba231959e78b75f
Saiful Islam <143730361+Saiful-Islam25@users.noreply.github.com>
2023-09-03 13:25:41 +0600

    Add files via upload

---
+++ Func_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void func();
+
+int main(){
+	for(int i=0;i<5;i++){
+	func();	
+	}
+	func();
+	
+	return 0;
+}
+void func(){
+	cout<<"Do not worry Nafis,Noor-Mohammad and Nusrat"<<endl;
+	}
+	                                                                         
