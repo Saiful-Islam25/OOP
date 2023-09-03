a804a3795af6e65a3498d5e145b0aab242464f75
Saiful Islam <143730361+Saiful-Islam25@users.noreply.github.com>
2023-09-03 13:27:51 +0600

    Add files via upload

---
+++ Palindrome.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+bool Palindrome(string str,int i,int j){
+    if(i>j){
+        return true;
+    }
+    if(str[i]!=str[j]){
+        return false;
+    }
+    else{
+        return Palindrome(str,i+1,j-1);
+    }
+}
+int main(){
+    string name = " BookkooB ";
+    cout<<endl;
+
+    bool isPalindrome=Palindrome(name,0,name.length()-1);
+
+    if(isPalindrome){
+        cout<<"Its a Palindrome"<<endl;
+    }else{
+        cout<<"Its not a Palindrome"<<endl;
+    }
+
+    return 0;
+}
